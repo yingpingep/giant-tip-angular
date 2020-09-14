bab613cc9d5480725437ba9ba4dd9a7644a4f0d7
yinnping <ciaoing@livemail.tw>
2020-09-15 01:13:57 +0800

    Update day4

--- day4/src/app/app.component.ts
+++ day4/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { inject, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'day4';
+  constructor(@Inject(DOCUMENT) private _document: any) {}
+  ngOnInit(): void {}
 }
