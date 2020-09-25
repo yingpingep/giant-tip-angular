a1d42f936e0f0dd91cecfa93e48dc2c360365c8f
yinnping <ciaoing@livemail.tw>
2020-09-26 00:36:34 +0800

    Add ICosService

--- day11/src/app/cos.service.ts
+++ day11/src/app/cos.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class CosService {
+export interface ICosService {
+  print(): void;
+}
 
-  constructor() { }
+@Injectable({
+  providedIn: 'root',
+})
+export class CosService implements ICosService {
+  constructor() {}
+
+  print(): void {
+    console.log(`⚡: CosService -> Original service`);
+  }
 }
