f25dca291272b7ceb23cd43507ac18cbf5639cfd
yinnping <ciaoing@livemail.tw>
2020-09-27 15:19:42 +0800

    Add dir

--- day12/src/app/app.module.ts
+++ day12/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MydirDirective } from './mydir.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MydirDirective
   ],
   imports: [
     BrowserModule
