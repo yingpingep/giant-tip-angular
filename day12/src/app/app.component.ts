427262a91b1af32501a151dfa00dcafafbb9e8fd
yinnping <ciaoing@livemail.tw>
2020-09-28 01:24:52 +0800

    Update for day13

--- day12/src/app/app.component.ts
+++ day12/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MydirDirective } from './mydir.directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('tMydir') mydir: MydirDirective;
   title = 'day12';
+  clickMe(): void {
+    this.mydir.add();
+  }
 }
--- day12/src/app/mydir.directive.ts
+++ day12/src/app/mydir.directive.ts
@@ -4,19 +4,23 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appMydir]',
+  exportAs: 'mydir'
 })
 export class MydirDirective implements OnInit, OnChanges {
   @Input() x: string;
+  count = 1;
   constructor(private ele: ElementRef<HTMLDivElement>) {}
   ngOnInit(): void {
-    const pEle = document.createElement('ol');
-    pEle.textContent = 'myDir works!';
-    this.ele.nativeElement.appendChild(pEle);
+    const litem = document.createElement('li');
+    litem.textContent = `init item ${this.count}`;
+    this.ele.nativeElement.appendChild(litem);
   }
   ngOnChanges(changes: SimpleChanges): void {
     console.log(`⚡: MydirDirective -> ngOnChanges -> this.x`, this.x);
   }
   add(): void {
-
+    const litem = document.createElement('li');
+    litem.textContent = `item ${++this.count}`;
+    this.ele.nativeElement.appendChild(litem);
   }
 }
