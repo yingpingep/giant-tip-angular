8fa48d66fb1e18a463f0c6983565305be9641738
yinnping <ciaoing@livemail.tw>
2020-09-30 02:31:50 +0800

    Update day15 example

--- day15/src/app/app.component.ts
+++ day15/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  someGroup: FormGroup;
   title = 'day15';
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.someGroup = this.fb.group({
+      aInput: new FormControl(),
+    });
+  }
+
+  onSubmit(): void {
+    console.log(`⚡: AppComponent -> onSumbit `, this.someGroup.value);
+  }
 }
