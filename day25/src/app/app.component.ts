caa9a526f9525d5c1ad7fcaa4c3e72217b6b84ee
yinnping <ciaoing@livemail.tw>
2020-10-10 17:53:14 +0800

    [Day 25] update

--- day25/src/app/app.component.ts
+++ day25/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { BehaviorSubject, interval, Subject } from 'rxjs';
+import { map, takeWhile, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'day25';
+  show = true;
+  interval1 = interval(1000);
+
+  interval2Value = 0;
+  interval2 = interval(1000);
+  constructor(private renderer: Renderer2) {}
+  ngOnInit(): void {
+    this.interval2.subscribe((x) => {
+      this.interval2Value = x;
+    });
+  }
+
+  changeShow(): void {
+    this.show = !this.show;
+  }
 }
